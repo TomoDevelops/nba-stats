b27aa531afa0a60dd2f3ad8d893d7acbd38a9756
Tomoki <tomoki_ota@outlook.com>
2023-10-07 16:57:42 +0900

    data filtering, chart output

--- src/views/TopReboundView.vue
+++ src/views/TopReboundView.vue
@@ -1,3 +1,45 @@
 <template>
-  <h1>Top Rebounds</h1>
+  <div class="flex flex-col items-center justify-center py-4 px-10 w-full">
+    <h2>Top 20 Rebounds in the '22 - '23 Season</h2>
+    <canvas id="top-rebound"></canvas>
+  </div>
 </template>
+
+<script>
+import { Chart } from 'chart.js/auto'
+
+export default {
+  data() {
+    return {
+      playerData: [],
+      playerName: [],
+      playerRebounds: [],
+      loading: false,
+      year: new Date().getFullYear()
+    }
+  },
+  methods: {
+    initChart() {
+      const canvas = document.getElementById('top-rebound')
+      new Chart(canvas, {
+        type: 'bar',
+        data: {
+          labels: [],
+          datasets: [
+            {
+              label: `Top Rebounds of ${this.year}`,
+              data: [],
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          scales: {
+            y: { beginAtZero: true }
+          }
+        }
+      })
+    }
+  }
+}
+</script>
--- src/views/TopScorerView.vue
+++ src/views/TopScorerView.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="flex flex-col items-center justify-center w-full">
+  <div class="flex flex-col items-center justify-center py-4 px-10 w-full">
     <h2>Top 20 Scorers in the '22 - '23 Season</h2>
-    {{ this.playerDataByScore }}
     <canvas id="top-scorer"></canvas>
   </div>
 </template>
 
 <script>
+import Chart from 'chart.js/auto'
+
 export default {
   data() {
     return {
       playerDataByScore: [],
+      playerName: [],
+      playerPoints: [],
       loading: false,
       year: new Date().getFullYear()
     }
@@ -18,13 +21,49 @@ export default {
   methods: {
     async getPlayerData() {
       this.loading = !this.loading
-      this.playerDataByScore = await fetch(
-        `https://nba-stats-db.herokuapp.com/api/playerdata/topscorers/total/season/${this.year}/`
-      )
-        .then((data) => data.json())
-        .then((res) => res.results.slice(0, 20))
-        .catch((err) => console.error(err.message))
-        .finally(() => (this.loading = !this.loading))
+      try {
+        const res = await fetch(
+          `https://nba-stats-db.herokuapp.com/api/playerdata/topscorers/total/season/${this.year}/`
+        )
+        const data = await res.json()
+        this.playerDataByScore = data.results.slice(0, 20)
+        this.getPlayerNamesAndPoints()
+        this.initChart()
+      } catch (err) {
+        console.error(err.message)
+      } finally {
+        this.loading = false
+      }
+    },
+    getPlayerNamesAndPoints() {
+      this.playerDataByScore.map((player) => {
+        this.playerName.push(player.player_name)
+        this.playerPoints.push(player.PTS)
+      })
+    },
+    initChart() {
+      const canvas = document.getElementById('top-scorer')
+      new Chart(canvas, {
+        type: 'bar',
+        data: {
+          labels: this.playerName,
+          datasets: [
+            {
+              label: `Top Scorers of ${this.year}`,
+              data: this.playerPoints,
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          indexAxis: 'y',
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      })
     }
   },
   created() {
